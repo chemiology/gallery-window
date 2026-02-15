31ab7f3267c6d4290c9323439076e2e40a2c0d59
chemiology <172052088+chemiology@users.noreply.github.com>
2026-02-15 16:58:30 +0900

    Delete css directory

--- css/js/guestbook-config.js
+++
@@ -1,2 +0,0 @@
-// Guestbook API endpoint (Google Apps Script Web App)
-window.GUESTBOOK_URL = 'https://script.google.com/macros/s/AKfycbxosBRSAcNGTRHQXIm6EIVQKEnFhFlK4jMGpIDFnccUfwXcLdRZO2vKn3GsmQvMjo8raQ/exec';
