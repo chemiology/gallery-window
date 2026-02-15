4bebd551017236aedac1c55484b11dadfd92ab25
chemiology <172052088+chemiology@users.noreply.github.com>
2026-02-15 17:29:46 +0900

    Add files via upload

--- js/hall.js
+++ js/hall.js
@@ -3,7 +3,7 @@ async function loadHall() {
   const params = new URLSearchParams(location.search);
   const hallId = params.get("hall");
 
-  const res = await fetch("data/hall.json");
+  const res = await fetch("./data/hall.json");
   const data = await res.json();
 
   const hall = data.halls.find(h => h.id === hallId);
